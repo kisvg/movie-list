45f0b1e924647eb1aa56483ef5e79ab8c0892b63
kisvg <kaisavage@gmail.com>
2024-09-08 01:11:57 -0700

    Update 1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+//firebase config
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-import { getFirestore, collection, getDocs, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAj4WhOtO_5fuUK7xqEU8ZOAlYrVTOx8uA",
   authDomain: "movie-list-9f569.firebaseapp.com",
@@ -19,7 +19,138 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
+
+//lists
+
+const omdbKeys = [
+  "f58b2e26",
+  "98d95a3b",
+  "c3abbed7",
+  "94c1da71",
+  "53538fbb"
+]
+
+const tmdbKeys = [
+  "74f95ee5445e918f8a2fba06cbcbdcc7",
+  "9ab9646452e3c0b56a4b2eb6817037e8",
+  "4f9b23de6f9cfe24882cd8fa423b1448",
+  "694837b1069fb93750e9d2393a64b682",
+  "aa451b89b21b649c47ad55247e48f25e"
+]
+
+const wmKeys = [
+  "R8V8CpBbYCv8zXXGfSU1VUDa812Y4KTF2rvFnjS1",
+  "yXhTyBaLxNQSs73zqKW0WeEoVcbX3HWpIqgV9fkz",
+  "5izGd0jD0X4lS2XAL2tu6p2J6BcEe8GhYkMljbsU",
+  "l9nHohIhu6uFmTBZKVvZNOGsbygHnEHyXKYmT1HB",
+  "VkogIv5ZnH48fVE08y56KzWDvge9gz7JakyJdJjG"
+]
+
+
+
+//functions
+
+function randKey(service) {
+  if (service == "omdb") {
+    var keyList = omdbKeys
+  }
+  if (service == "tmdb") {
+    var keyList = tmdbKeys
+  }
+  if (service == "wm") {
+    var keyList = wmKeys
+  }
+  return (keyList[Math.floor(Math.random()*(keyList.length))])
+}
+
+async function addMovie(name) {
+  //var newMovie = {};
+  await getOmdb(name);
+  
+  try {
+    //console.log(newMovie);
+    const docRef = await addDoc(collection(db, "unwatched"), newMovie);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+  }
+;
+
+var newMovie = {};
+
+// Make a GET request
+async function getOmdb(name) {
+  fetch("https://www.omdbapi.com/?t="+name+"&plot=full&apikey="+randKey("omdb"))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      //do stuff w/ data
+      //console.log(data);
+      newMovie["name"] = name;
+      newMovie["csrating"] = "";
+      newMovie["imdbid"] = data.imdbID;
+      newMovie["poster"] = data.Poster;
+      newMovie["runtime"] = data.Runtime;
+      newMovie["type"] = data.Type;
+      newMovie["plot"] = data.Plot;
+      // finds object w/ rt rating
+      const rtobj = data.Ratings.find(r => r.Source === "Rotten Tomatoes");
+      newMovie["rtrating"] = rtobj ? rtobj.Value : null;
+      document.getElementById("text2").innerHTML = (newMovie.imdbid);
+      document.getElementById("poster").src = (newMovie.poster);
+      getTmdb(data.imdbID)
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+
+async function getTmdb(imdbid) {
+  fetch("https://api.themoviedb.org/3/movie/"+imdbid+"/watch/providers?api_key="+randKey("tmdb"))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(initialdata => {
+      const data = initialdata.results.US.flatrate
+      //do stuff w/ data
+      var services = [];
+      data.forEach(item => {
+        services.push(item.provider_name)
+      });
+      newMovie["services"] = services;
+      console.log(newMovie);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+};
+
+
+
+
+getOmdb("her")
+// await addMovie("bambi")
+
+
+
+
+
 //code
+
+// add movie
+// await addMovie("Mathilda");
+
+// set movie
+
+/*
 try {
     const docRef = await setDoc(doc(db, "unwatched","movieid"), {
       name: "Dune",
@@ -30,9 +161,14 @@ try {
   } catch (e) {
     console.error("Error adding document: ", e);
   };
+*/
 
-const querySnapshot = await getDocs(collection(db, "unwatched"));
-querySnapshot.forEach((doc) => {
+// get
+
+/*
+const dataSnapshot = await getDocs(collection(db, "unwatched"));
+dataSnapshot.forEach((doc) => {
   console.log(` imdbid: ${doc.id} \n name: ${doc.data().name}`);
   document.getElementById("text").innerHTML =(` imdbid: ${doc.id}, name: ${doc.data().name}`)
 });
+*/
